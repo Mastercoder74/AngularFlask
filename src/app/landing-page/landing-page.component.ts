33db7a0619a6569952dd86b7440c519204781ca0
Mastercoder74 <sriharish74@gmail.com>
2021-04-18 15:56:51 +0530

    Ch

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -11,36 +11,40 @@ export class LandingPageComponent implements OnInit {
 
   inputString: string[];
 
-  radioOption1: string;
-  radioOption2: string;
-  radioOption3: string;
-  radioOption4: string;
+  radioOption1: string[];
+  radioOption2: string[];
+  radioOption3: string[];
+  radioOption4: string[];
 
-  multiOption1:string;
-  multiOption2:string;
-  multiOption3:string;
-  multiOption4:string;
+  multiOption1:string[];
+  multiOption2:string[];
+  multiOption3:string[];
+  multiOption4:string[];
 
   iValue: number;
 
   num = 5;
 
   formArray: Array<string>;
+  optionsArray: Array<string>;
 
   constructor()
   {
     this.formArray = [];
+    this.optionsArray = [];
     this.inputString = [];
     this.questionType = [];
     this.iValue = 0;
-    this.radioOption1 = '';
-    this.radioOption2 = '';
-    this.radioOption3 = '';
-    this.radioOption4 = '';
-    this.multiOption1 = '';
-    this.multiOption2 = '';
-    this.multiOption3 = '';
-    this.multiOption4 = '';
+
+    this.radioOption1 = [];
+    this.radioOption2 = [];
+    this.radioOption3 = [];
+    this.radioOption4 = [];
+
+    this.multiOption1 = [];
+    this.multiOption2 = [];
+    this.multiOption3 = [];
+    this.multiOption4 = [];
   }
 
   questionType: string[];
@@ -81,10 +85,22 @@ export class LandingPageComponent implements OnInit {
   {
     let formLength = this.formArray.length
     this.formArray = [];
+    this.optionsArray = [];
 
     for(let i = 0; i < formLength; i++)
     {
       this.formArray.push(this.inputString[i]);
+
+        this.optionsArray.push(this.radioOption1[i])
+        this.optionsArray.push(this.radioOption2[i])
+        this.optionsArray.push(this.radioOption3[i])
+        this.optionsArray.push(this.radioOption4[i])
+
+        this.optionsArray.push(this.multiOption1[i])
+        this.optionsArray.push(this.multiOption2[i])
+        this.optionsArray.push(this.multiOption3[i])
+        this.optionsArray.push(this.multiOption4[i])
+
     }
 
   }
